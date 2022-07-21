0affc5c6333132a7cd665f218af98e5cf0cea46a
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-07-21 19:27:01 +0530

    Longest Consecutive Sequence

--- solution.cpp
+++ solution.cpp
@@ -1,36 +1,35 @@
 #include<bits/stdc++.h>
-#include "print.cpp"
+#include<iostream>
 using namespace std;
 
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-      if(intervals.size() == 0) return 0;
-      auto comparator = [](const vector<int>& a, const vector<int>& b) {
-        return a[1] < b[1];
-      };
-      sort(intervals.begin(), intervals.end(), comparator);
-      int end = intervals[0][1];
-      int count = 1;
-
-      for(int i=1; i<intervals.size(); i++) {
-        if(intervals[i][0] >= end) {
-          ++count;
-          end = intervals[i][1];
+    int longestConsecutive(vector<int> nums) {
+        if(nums.size()==0) return 0;
+        sort(nums.begin(),nums.end());
+        int sequence_size = 1;
+        int max = 0;
+        int previous = nums[0];
+        for(int num : nums) {
+            if(num-previous == 1) ++sequence_size;
+            else if(num == previous) continue;
+            else {
+                if(sequence_size > max)
+                    max = sequence_size;
+                sequence_size = 1;
+            }
+            previous = num;
         }
-      }
-      return intervals.size() - count;
+        if(sequence_size > max)
+            max = sequence_size;
+        return max;
     }
 };
 
+
 int main() {
-  vector<vector<int>> intervals = {
-    {1,3},
-    {1,2},
-    {2,3},
-    {3,4}
-  };
-  Solution s;
-  cout << s.eraseOverlapIntervals(intervals);
-  return 0;
+    Solution s;
+    int size = s.longestConsecutive({0,1,1,2,3});
+    cout<<size<<endl;
+    return 0;
 }
