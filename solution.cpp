90ced2400d4a6b21c811f37ad07f6f3a5e0b9fb4
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-07-25 21:02:50 +0530

    Container with most water

--- solution.cpp
+++ solution.cpp
@@ -4,28 +4,14 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int> nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
-        int size = nums.size();
-        for(int i=0;i<size-1;i++) {
-            int target = -nums[i];
-            int begin = i+1;
-            int end = size-1;
-
-            while(begin < end) {
-                int sum = nums[begin] + nums[end];
-                if(sum < target) ++begin; 
-                else if(sum > target) --end;
-                
-                else {
-                    vector<int> entry = {nums[i], nums[begin], nums[end]};
-                    res.push_back(entry);
-                    while(begin < end && entry[1] == nums[begin]) ++begin;
-                    while(begin < end && entry[2] == nums[end]) --end;
-                }
-            }
-            while(i + 1 < size && nums[i] == nums[i+1]) ++i;
+    int maxArea(vector<int> height) {
+        int res = 0;
+        int begin = 0, end = height.size()-1;
+        while(begin < end) {
+            int area = (end-begin) * min(height[end],height[begin]);
+            if(area > res) res = area;
+            if(height[begin] < height[end]) ++begin;
+            else --end;
         }
         return res;
     }
@@ -34,6 +20,7 @@ public:
 
 int main() {
     Solution s;
-    auto v = s.threeSum({-1,0,1,2,-1,-4});
+    int area = s.maxArea({1,8,6,2,5,4,8,3,7});
+    cout<<"max area : "<<area<<endl;
     return 0;
 }
