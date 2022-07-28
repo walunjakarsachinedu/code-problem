f9b237f267f2bb22e8b4e3bf2fe82bbb6b6b5b14
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-07-28 06:05:15 +0530

    Best Time to Buy & Sell Stock

--- solution.cpp
+++ solution.cpp
@@ -4,46 +4,16 @@ using namespace std;
 
 class Solution {
 public:
-    // 0,1,0,2,1,0,1,3,2,1,2,1
-    int trapWithoutTwoPointer(vector<int> height) {
-        int size = height.size();
-        if(size < 3)  return 0;
-        
-        int total_water = 0;
-        vector<int> lMax,rMax;
-        int lm = height[0], rm = height[size-1];
-        for(int i=0;i<size;i++) {
-            lm = max(lm, height[i]);
-            rm = max(rm, height[size-1-i]);
-            lMax.push_back(lm);
-            rMax.push_back(rm);
+    int maxProfit(vector<int>& prices) {
+        int l = 0;
+        int r = 1;
+        int max_profit = 0;
+        while(r < prices.size()) {
+            if(prices[r] > prices[l]) max_profit = max(prices[r] - prices[l],max_profit);
+            else l = r;
+            r++;
         }
-        for(int i=1;i<size-1;i++) {
-            int water = min(lMax[i-1],rMax[size-1-i]) - height[i];
-            total_water += max(water, 0);
-        }
-        return total_water;
-    }
-
-    int trap(vector<int> height) {
-        int size = height.size();
-        if(size < 3) return 0;
-        int l = 0, ml = height[0];
-        int r = size-1, mr = height[size-1];
-        int water = 0;
-        
-        while(l <= r) { //2,0,2
-            if(height[l] <= height[r]) {
-                if(height[l] > ml) ml = height[l];
-                else water += ml-height[l];
-                l++;
-            } else {
-                if(height[r] > mr) mr = height[r];
-                else water += mr-height[r];
-                r--;
-            }
-        }
-        return water;
+        return max_profit;
     }
 };
 
@@ -51,7 +21,8 @@ public:
 
 int main() {
     Solution s;
-    int total = s.trap({4,2,0,3,2,5});
-    cout<<"water : "<<total<<endl;
+    vector<int> prices = {1,2,4,2,5,7,2,4,9,0,9};
+    int total = s.maxProfit(prices);
+    cout<<"profit : "<<total<<endl;
     return 0;
 }
