cd24a60bc39d18e4e790bee176ab434abc957d92
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-02 18:21:35 +0530

    74. Search a 2D Matrix

--- solution.cpp
+++ solution.cpp
@@ -6,7 +6,7 @@ public:
     int search(vector<int> nums, int target) {
         int l = 0, r = nums.size()-1;
         while(l <= r) {
-            int mid = r-l/2 + l;
+            int mid = (r-l)/2 + l;
             if(target == nums[mid]) return mid;
             else if(target < nums[mid]) {
                 r = --mid;
@@ -16,11 +16,33 @@ public:
         }
         return -1;
     }
+
+    bool searchMatrix(vector<vector<int>> matrix, int target) {
+        int m=matrix.size(), n=matrix[0].size();
+        int l=0, r=m-1;
+        while(l<=r) {
+            if(l==r) break;
+            int mid = (r-l)/2 + l;
+            if(target == matrix[mid][n-1]) return true;
+            else if(target < matrix[mid][n-1]) {
+                r = --mid;
+            } else {
+                l = ++mid;
+            }
+        }
+        if(target > matrix[l][n-1])
+            ++l;
+
+        if(l < m) {
+            return search(matrix[l], target) != -1;
+        }
+        return false;
+    }
 };
 
 int main() {
     Solution s;
-    int position = s.search({-1,0,3,5,9,12},2);
-    cout<<"position: "<<position<<endl;
+    bool ispresent = s.searchMatrix({{1,3,5,7},{10,11,16,20},{23,30,34,60}},34);
+    cout<<"isPresent: "<<(ispresent ? "true" : "false")<<endl;
     return 0;
 }
