9d6ae8d11fc94e5ff4ffd97c85cf27dec23b25fe
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-04 14:48:18 +0530

    981. Time Based Key-Value Store

--- solution.cpp
+++ solution.cpp
@@ -1,39 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class TimeMap {
+    unordered_map<string, vector<pair<string,int>>> timeMap;
 public:
-    /* 
-        eliminate the sorted part 
-        then you get unsorted part
-        return minimum of unsorted part
-    */
-    int findMin(vector<int> nums) {
-        int size = nums.size();
-        int l=0, r=size-1;
+    TimeMap() {}
+    
+    void set(string key, string value, int timestamp) {
+        timeMap[key].push_back({value, timestamp});
+    }
 
-        if(nums[l] <= nums[r]) return nums[l];
-
-        while(l<r) {
-            int mid = (r-l)/2 + l;
-            if(nums[l] <= nums[mid]) {
-                l = mid+1;
-                if(l < size && nums[mid] > nums[l])
-                    return nums[l];
-            } 
-            else {
-                r = mid-1;
-                if(r >= 0 && nums[r] > nums[mid])
-                    return nums[mid];
+    // uses binary search for getting value, so time complexity is O(logn)
+    string get(string key, int timestamp) {
+        auto& values = timeMap[key];
+        int l=0,r=values.size()-1;
+        string res;
+        while(l<=r) {
+            int m = (r-l)/2+l;
+            if(timestamp >= values[m].second) {
+                res = values[m].first;
+                l=m+1;
+            } else {
+                r=m-1;
             }
         }
-        return -1;
+        return res;
     }
 };
 
+
 int main() {
-    Solution s;
-    int min_value = s.findMin({11,13,15,17});
-    cout << "min_value : " << min_value << endl;
+    TimeMap timeMap;
+    timeMap.set("foo", "bar", 1);  // store the key "foo" and value "bar" along with timestamp = 1.
+    timeMap.get("foo", 1);         // return "bar"
+    timeMap.get("foo", 3);         // return "bar", since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 is "bar".
+    timeMap.set("foo", "bar2", 4); // store the key "foo" and value "bar2" along with timestamp = 4.
+    timeMap.get("foo", 4);         // return "bar2"
+    timeMap.get("foo", 5);         // return "bar2"
     return 0;
 }
