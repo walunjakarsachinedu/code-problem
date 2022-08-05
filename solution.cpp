d90cf96f0392ac6aae9f8307a00ab5830f45e2d2
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-05 07:17:46 +0530

    143. Reorder List

--- solution.cpp
+++ solution.cpp
@@ -32,30 +32,40 @@ using namespace std;
 
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode *dummy = new ListNode();
-        ListNode *tail = dummy;
+    void reorderList(ListNode* head) {
+        ListNode *sp = head, *fp = head->next; //sp: slow pointer, fp: fast pointer
+        while(fp && fp->next) sp = sp->next, fp= fp->next->next;
 
-        while(list1 && list2) {
-            if(list1->val > list2->val) swap(list1,list2); // ensure list1 value is smaller than list2
-            tail->next = list1;
-            list1 = list1->next;
-            tail = tail->next;
+        ListNode *list = head;
+        ListNode *rlist = reverseList(sp->next);
+        sp->next = nullptr;
+
+        while(rlist && list) {
+            ListNode *lnext = list->next, *rlnext = rlist->next;
+            list->next = rlist;
+            rlist->next = lnext;
+            list = lnext;
+            rlist = rlnext;
         }
+    }
 
-        tail->next = (list1) ? list1 : list2;
-
-        return dummy->next;
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = nullptr;
+        while(head) {
+            ListNode* headNext = head->next;
+            head->next = prev;
+            prev = head;
+            if(headNext == nullptr) break;
+            head = headNext;
+        }
+        return head;
     }
 };
 
 int main() {
     Solution s;
-    ListNode* list1 = new ListNode({1,2,4,5,7});
-    ListNode* list2 = new ListNode({1,3,3,6});
-    // list1->print();
-    // list2->print();
-    s.mergeTwoLists(list1,list2)->print();
+    ListNode* list1 = new ListNode({1,2,4,5,7,8});
+    s.reorderList(list1);
     return 0;
 }
 
