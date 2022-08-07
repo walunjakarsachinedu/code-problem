69b285ef52a3b77eab95b5cce80e461c3479bec9
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-07 11:38:58 +0530

    23. Merge k Sorted Lists

--- solution.cpp
+++ solution.cpp
@@ -1,108 +1,68 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-struct Node {
-    int val;
-    int key;
-    Node* prev;
-    Node* next;
-    Node() {}
-    Node(int key, int val) : val(val), key(key), prev(nullptr), next(nullptr)  {}
-
-    ///Order of parameter matter most in this function
-    static void addLink(Node* a, Node* b) {
-        if(a) a->next = b;
-        if(b) b->prev = a;
-    }
-};
-
-struct LinkedList {
-    Node* front;
-    Node* back;
-    LinkedList(): front(nullptr), back(nullptr) {}
-
-    void addToBack(Node* node) {
-        Node::addLink(back, node);
-        back = node;
-        node->next = nullptr;
-        if(front == nullptr) front = back;
-    }
-
-    void moveToBack(Node* node) {
-        if (node != back) { // moving node to back
-            if (node == front) front = front->next;
-            else Node::addLink(node->prev, node->next); // removing element
-            addToBack(node);
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+     ListNode(vector<int> values) {
+        vector<ListNode*> nodes;
+        for(int i=0;i<values.size();i++) {
+            nodes.push_back(new ListNode(values[i]));
+            if(i>0) nodes[i-1]->next = nodes[i];
         }
-    }
+        val = nodes[0]->val;
+        next = nodes[0]->next;
+     }
 
-    void print(bool forward) {
-        if(forward) {
-            Node* node = front;
-            while(node) {
-                cout<<"("<<node->key<<":"<<node->val<<") ";
-                node = node->next;
-            }
-        } else {
-            Node* node = back;
-            while(node) {
-                cout<<node->val<<" ";
-                node = node->prev;
-            }
+     void print() {
+        ListNode* node = this;
+        while(node) {
+            cout<<node->val<<" ";
+            node = node->next;
         }
-        cout<<endl;
-    }
-};
-// most recently used node is  most recently add, updated, getted
-class LRUCache {
+     }
+ };
+
+class Solution {
 public:
-    int capacity;
-    unordered_map<int,Node*> hashMap;
-    LinkedList queue; // most recently used node is at back of queue
-
-    LRUCache(int capacity) : capacity(capacity) {}
-    
-    int get(int key) {
-        if(hashMap.find(key) == hashMap.end()) return -1;
-
-        Node* node = hashMap[key];
-        queue.moveToBack(node);
-        return node->val;
+    ListNode* mergeKLists(vector<ListNode*> lists) {
+        int l = 0, r = lists.size()-1;
+        while(r>0) {
+            while(l<r) {
+                lists[l] = mergeTwoList(lists[l], lists[r]);
+                l++, r--;
+            }
+            l=0;
+        }
+        return lists[0];
     }
-    
-    void put(int key, int value) {
-        //updating value
-        if (hashMap.find(key) != hashMap.end()) {
-            Node* node = hashMap[key];
-            node->val = value;
-            queue.moveToBack(node);
-            return ;
-        }
 
-        Node* node = new Node(key, value);
-        //resizing queue for adding new element
-        if(hashMap.size() >= capacity) { //capacity > 0
-            hashMap.erase(queue.front->key); //remove from hashmap
-            queue.front = queue.front->next; //remove from queue
+    ListNode* mergeTwoList(ListNode* list1, ListNode* list2) {
+        ListNode* mergeList = new ListNode();
+        ListNode* tail = mergeList;
+        while(list1!= nullptr && list2 != nullptr) {
+            if(list1->val < list2->val) {
+                tail->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                tail->next = list2;
+                list2 = list2->next;
+            }
+            tail = tail->next;
         }
-        queue.addToBack(node);
+        tail->next = (list1) ? list1 : list2;
 
-        if(queue.back == nullptr) queue.back = queue.front;
-        hashMap[key] = node;
+        return mergeList->next;
     }
 };
+
 
 int main() {
-    LRUCache lRUCache = LRUCache(2);
-    //[2],[2,1],[1,1],[2,3],[4,1],[1],[2]
-    lRUCache.put(2,1);
-    lRUCache.put(1,1);
-    lRUCache.put(2,3);
-    lRUCache.put(4,1);
-    //lRUCache.queue.print(true);
-    lRUCache.get(1);
-    cout<<lRUCache.get(2);
+    Solution s;
+    s.mergeKLists({new ListNode({1,5,10}),new ListNode({1,2,3}), new ListNode({0,2,3,4})})->print();
     return 0;
 }
-
