2f10ffbb859944ebf9e12d6f8ff7c6868beb8b14
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-09 05:21:17 +0530

    102. Binary Tree Level Order Traversal

--- solution.cpp
+++ solution.cpp
@@ -25,27 +25,39 @@ private:
 
 class Solution {
 public:
-    bool isSubtree(TreeNode *root, TreeNode *subRoot) {
-        if(root==nullptr) return false;
-        return isSameTree(root, subRoot) 
-        || isSubtree(root->left, subRoot) 
-        || isSubtree(root->right, subRoot);
-    }
-
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==nullptr && q==nullptr) return true;
-        return (p && q) 
-        && (p->val == q->val) 
-        && isSameTree(p->left, q->left) 
-        && isSameTree(p->right, q->right);
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode*> que;
+        vector<vector<int>> res;
+        if(root) que.push(root);
+        while(!que.empty()) {
+            vector<int> level;
+            int size = que.size();
+            for(int i=0;i<size;i++) {
+                TreeNode* node = que.front();
+                level.push_back(node->val);
+                if(node->left) que.push(node->left);
+                if(node->right) que.push(node->right);
+                que.pop();
+            }
+            res.push_back(level);
+        }
+        return res;
     }
 };
 
 
 int main() {
     Solution s;
-    TreeNode* root1 = new TreeNode(1,new TreeNode(2), new TreeNode(1));
-    TreeNode* root2 = new TreeNode(1,new TreeNode(1), new TreeNode(2));
-    cout<<(s.isSubtree(root1,root2) ? "tree contains subtree" : "tree doesn't contains subtree")<<endl;
+    TreeNode three = TreeNode(3), five = TreeNode(5);
+    TreeNode zero = TreeNode(0), four = TreeNode(4, &three, &five), seven = TreeNode(7), nine = TreeNode(9);
+    TreeNode two = TreeNode(2, &zero, &four), eight = TreeNode(8, &seven, &nine);
+    TreeNode *root = new TreeNode(6, &two, &eight);
+    auto result = s.levelOrder(nullptr);
+    for(auto level : result) {
+        for(auto node : level) {
+            cout<<node<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
