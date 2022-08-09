abbda20f5ccd8c95c9dc071a21b8c5c2e2afc8ca
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-09 08:40:49 +0530

    230. Kth Smallest Element in a BST

--- solution.cpp
+++ solution.cpp
@@ -29,27 +29,51 @@ private:
 
 class Solution {
 public:
-    bool isValidBST(TreeNode *root) {
-        bool isTreeValid = true;
-        return _isValidBST(root, LONG_MAX, LONG_MIN);
+    // iterative
+    int kthSmallestIterative(TreeNode* root, int k) {
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        while(node != nullptr) {
+            st.push(node);
+            node = node->left;
+        }
+        while(k!=0 && !st.empty()) {
+            node = st.top();
+            st.pop(); --k;
+            if(k == 0) return node->val;
+            node = node->right;
+            while(node != nullptr) {
+                st.push(node);
+                node = node->left;
+            }
+        }
+        return 0;
     }
-private:
-    bool _isValidBST(TreeNode *root, long int minRange, long int maxRange) {
-        if (root == nullptr) return true;
-        if (root->val <= minRange || root->val >= maxRange) return false;
-        if (_isValidBST(root->left, minRange, root->val) 
-        && _isValidBST(root->right, root->val, maxRange)) return true;
-        return false;
+
+    // recursive optimize
+    int kthSmallest(TreeNode* root, int k) {
+        int smallestValue = 0;
+        _kthSmallest(root, k, smallestValue);
+        return smallestValue;
     }
+
+    int _kthSmallest(TreeNode* root, int &k, int &value) {
+        if(root==nullptr || k<0) return 0;
+        _kthSmallest(root->left,k, value);
+        --k;
+        if(k==0) value = root->val;
+        _kthSmallest(root->right,k, value);
+        return 0;
+    }
+
 };
 
 int main() {
     Solution s;
-    TreeNode zero(0), one(1), two(2),three(3), four(4), five(5), six(6), seven(7), eight(8), nine(9);
+    TreeNode zero(0), one(1), two(2),three(3), four(4), five(5), six(6), seven(7), eight(8), nine(9), ten(10);
     six.link(&two,&eight);
-    two.link(&zero, &four); eight.link(&seven, &nine);
+    two.link(&zero, &four); eight.link(&seven, &ten);
     four.link(&three, &five);
-    if(s.isValidBST(&five)) cout<<"binary tree is valid"<<endl;
-    else cout<<"binary tree is in-valid"<<endl;
+    cout<<s.kthSmallest(&six,5)<<endl;
     return 0;
 }
