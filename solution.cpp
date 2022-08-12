9a801d569dd876cbf49f841c4276c0b3d80c0f9a
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-12 10:52:59 +0530

    703. Kth Largest Element in a Stream

--- solution.cpp
+++ solution.cpp
@@ -1,88 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct Node {
-    Node* children[26];
-    bool isEnd;
-    Node(char val=' ') : isEnd(false) {
-        for(int i=0;i<26;i++) children[i] = nullptr;
-    }
-
-};
-
-class Tries {
+class KthLargest {
 public:
-    Node* head = new Node();
-    Tries() {}
-    Tries(vector<string>& words) {
-        for(string word : words) addWord(word);
-    }
-
-    void addWord(string word) {
-        Node* node = head;
-        for(int i=0;i<word.size();i++) {
-            if(node->children[word[i]-97]==nullptr) node->children[word[i]-97] = new Node(word[i]);
-            node = node->children[word[i]-97];
+    vector<int> heap;
+    int k;
+    KthLargest(int k, vector<int> nums): k(k) {
+        heap = vector<int>(nums.begin(), nums.end());
+        make_heap(heap.begin(), heap.end(), greater<int>());
+        while(heap.size() > k) {
+            pop_heap(heap.begin(), heap.end(), greater<int>());
+            heap.pop_back();
         }
-        node->isEnd = true;
     }
-
-    bool search(string word) {
-        Node* node = head;
-        for(int i=0;i<word.size();i++) {
-            if(node->children[word[i]-97] == nullptr) return false;
-            node = node->children[word[i]-97];
+    
+    int add(int val) {
+        if(heap.size() >= k) {
+            if(val > heap.front()) {
+                pop_heap(heap.begin(), heap.end(), greater<int>());
+                heap.pop_back();
+            } 
+            else return heap.front();
         }
-        return node->isEnd;
-    }
-};
-
-class Solution {
-public:
-    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        Tries trie(words);
-        for(int i=0;i<board.size();i++) {
-            for(int j=0;j<board[0].size();j++) {
-                if(i==1 && j==3) {
-                    int a= 3;
-                }
-                _findWords(board, trie.head, j, i, "");
-                if(result.size() == words.size()) return words;
-            }
-        }
-        return vector<string>(result.begin(), result.end());
-    }
-
-private:
-    set<string> result;
-    void _findWords(vector<vector<char>>& board, Node* node, int x, int y, string matched_word) {
-        if(y<0 || x<0 || x==board[0].size() || y==board.size() || board[y][x]==' ') return;
-        char ch = board[y][x];
-
-        if(node->children[ch-97] == nullptr) return;
-
-        node = node->children[ch-97];
-        matched_word += ch;
-        board[y][x]=' ';
-        if(node->isEnd) result.insert(matched_word);
-
-        _findWords(board, node, x-1, y, matched_word);
-        _findWords(board, node, x+1, y, matched_word);
-        _findWords(board, node, x, y-1, matched_word);
-        _findWords(board, node, x, y+1, matched_word);
-        board[y][x]=ch;
+        heap.push_back(val);
+        push_heap(heap.begin(), heap.end(), greater<int>());
+        return heap.front();
     }
 };
 
 int main() {
-    Solution s;
-    vector<vector<char>> board = {{'a', 'b'}};
-    vector<string> words = {"ba"};
-    vector<string> word_found = s.findWords(board, words);
-    for(string word : word_found) cout<<word<<" ";
-    cout<<endl;
+    KthLargest kthLargest(2, {0});
+    cout<<kthLargest.add(-1)<<endl;
+    cout<<kthLargest.add(1)<<endl;
+    cout<<kthLargest.add(-2)<<endl;
+    cout<<kthLargest.add(-4)<<endl;
+    cout<<kthLargest.add(3)<<endl;
     return 0;
 }
-
- 
- 
