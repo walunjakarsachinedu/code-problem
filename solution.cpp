f5b7f303d3389c71fa6a5422ec9e9392883eae12
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-13 15:06:43 +0530

    621. Task Scheduler

--- solution.cpp
+++ solution.cpp
@@ -3,31 +3,32 @@ using namespace std;
 
 class Solution {
 public:
-    int findKthLargest(vector<int> nums, int k) {
-        k = nums.size() - k;
-        return quickSelect(nums, 0, nums.size()-1, k);
-    }
-    
-    int quickSelect(vector<int>& nums, int l, int r, int k) {
-        int pivot = nums[r];
-        int p = l;
-        for(int i=l;i<=r;i++) {
-            if(nums[i] < pivot) {
-                swap(nums[i], nums[p]);
-                ++p;
+    int leastInterval(vector<char> tasks, int n) {
+        int freq[26];
+        for(int i=0;i<26;i++) freq[i] = 0; 
+        for(char task : tasks) ++freq[task-'A'];
+
+        int time = 0;
+        priority_queue<int> taskQue;
+        for (int f : freq) if (f != 0) taskQue.push(f);
+        queue<pair<int,int>> upcomingTask; // task & time
+        while(!taskQue.empty() || !upcomingTask.empty()) {
+            if (!upcomingTask.empty() && upcomingTask.front().second <= time) {
+                taskQue.push(upcomingTask.front().first);
+                upcomingTask.pop();
+            }
+            time +=1; // timer
+            if(!taskQue.empty()) {
+                int task = taskQue.top() - 1; taskQue.pop();
+                if(task > 0) upcomingTask.push({task, time + n});
             }
         }
-        swap(nums[r],nums[p]);
-        if(k == p) return nums[k];
-        if(k < p) return quickSelect(nums, l, p-1, k);
-        else return quickSelect(nums, p+1, r, k);
-    
-        return 0;
+        return time;
     }
 };
 
 int main() {
     Solution s;
-    cout<<s.findKthLargest({1,1}, 1)<<endl;
+    cout<<s.leastInterval({'A','A','A','B','B','B'}, 2)<<endl;
     return 0;
 }
