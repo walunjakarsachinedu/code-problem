dfd3277a027913fda671a274a918353ff74aa401
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-15 11:33:38 +0530

    40. Combination Sum II

--- solution.cpp
+++ solution.cpp
@@ -4,32 +4,32 @@ using namespace std;
 class Solution {
     vector<vector<int>> result;
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<int> permutation;
-        _subsetsWithDup(nums, 0, permutation);
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int> combination;
+        _combintationSum2(candidates, 0, target, combination);
         return result;
     }
 
-    void _subsetsWithDup(vector<int>& nums, int i, vector<int>& permutation) {
-        result.push_back(permutation);
-        set<int> visited;
-        for(int j=i;j<nums.size();j++) {
-            if(visited.find(nums[j]) == visited.end()) {
-                visited.insert(nums[j]);
-                permutation.push_back(nums[j]);
-                _subsetsWithDup(nums, j+1, permutation);
-                permutation.pop_back(); // backtracking
-            }
+    void _combintationSum2(vector<int>& candidates, int i, int target, vector<int>& combination) {
+        if(target == 0) result.push_back(combination);
+        if(target <= 0) return ; // base condition
+        if(i<candidates.size() && candidates[i] > target) return ;
+
+        int prev = -1; 
+        for(int j=i;j<candidates.size();j++) {
+            if(candidates[j] == prev) continue; // eliminate dublicate
+            combination.push_back(candidates[j]);
+            _combintationSum2(candidates, j+1, target-candidates[j], combination);
+            combination.pop_back(); // backtracking
+            prev = candidates[j];
         }
     }
 };
 
 int main() {
     Solution s;
-    vector<int> nums = {1,1,2,2};
-    s.subsetsWithDup(nums);
+    vector<int> nums = {1,1,1,3,3,5};
+    s.combinationSum2(nums, 8);
     return 0;
 }
-
-// [[], [1], [2], [1,1], [2,2], [1,2], [1,1,2], [1,2,2], [1,1,2,2]]
