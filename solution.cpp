9d67f877cd63a38fa5f590fc2b2973fa4395827f
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-15 05:58:11 +0530

    39. Combination Sum

--- solution.cpp
+++ solution.cpp
@@ -1,33 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/*
-1,2,3
-        
-    {1}     {}
-
-*/
 class Solution {
-    vector<vector<int>> result;
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        _subsets(nums, 0, {});
+    vector<vector<int>> result;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> combination;
+        _combinationSum(candidates, 0, target, combination);
         return result;
     }
-    void _subsets(vector<int>& nums, int i, vector<int> collection) {
-        result.push_back(collection);
-        for(int j=i;j<nums.size();j++) {
-            vector<int> new_collection(collection);
-            new_collection.push_back(nums[j]);
-            _subsets(nums, j+1, new_collection);
+
+    void _combinationSum(vector<int>& candidates, int i, int target, vector<int>& combination) {
+        if(target == 0) result.push_back(combination);
+        if(target <= 0) return;
+
+        combination.push_back(candidates[i]); // adding same element (repetition)
+        _combinationSum(candidates, i, target-candidates[i], combination);
+        combination.pop_back();
+
+        for(int j=i+1;j<candidates.size();j++) {
+            combination.push_back(candidates[j]); // adding new element
+            _combinationSum(candidates, j, target-candidates[j], combination);
+            combination.pop_back();
         }
     }
 };
 
 int main() {
     Solution s;
-    vector<int> nums = {};
-    s.subsets(nums);
-
+    vector<int> candidates = {2,3,5};
+    s.combinationSum(candidates, 8);
     return 0;
 }
