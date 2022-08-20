e0a9fb3d8fd3b3f94e5e082d9f81009c416da936
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-20 17:06:59 +0530

    130. Surrounded Regions

--- solution.cpp
+++ solution.cpp
@@ -2,50 +2,45 @@
 using namespace std;
 
 class Solution {
-    int m, n; // m -> height, n -> width   (of matrix)
+    int m, n; // m -> height, n -> width   (of board)
 public:
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        set<pair<int,int>> pacificOcean, atlanticOcean;
-        m=heights.size(), n=heights[0].size();
+    void solve(vector<vector<char>>& board) {
+        m=board.size(), n=board[0].size();
         
-        for(int x=0; x<n; x++) {
-            dfs(heights, x, 0, pacificOcean, heights[0][x]);
-            dfs(heights, x, m-1, atlanticOcean, heights[m-1][x]);
-        }
-        for(int y=0; y<m; y++) {
-            dfs(heights, 0, y, pacificOcean, heights[y][0]);
-            dfs(heights, n-1, y, atlanticOcean, heights[y][n-1]);
-        }
+        for(int x=0; x<n; x++) capture(board, x, 0, 'T'), capture(board, x, m-1, 'T');
+        for(int y=0; y<m; y++) capture(board, 0, y, 'T'), capture(board, n-1, y, 'T');
         
-        vector<vector<int>> result;
-        for(auto i : pacificOcean) if(atlanticOcean.find(i) != atlanticOcean.end()) {
-            result.push_back({i.second, i.first});
-        }
-        return result;
+        for(int y=0; y<m; y++) for(int x=0; x<n; x++) capture(board, x, y);        
+        
+        for(int y=0; y<m; y++) for(int x=0; x<n; x++) if(board[y][x]=='T') board[y][x] = 'O';        
     }
     
-    void dfs(vector<vector<int>>& heights, int x, int y, set<pair<int,int>>& visited, int prevHeight) {
-        if(x<0 || y<0 || x>=n || y>=m || visited.find({x,y})!=visited.end() || heights[y][x] < prevHeight) return;
+    void capture(vector<vector<char>>& board, int x, int y, char ch='X', char r_ch='O') { 
+        if(x<0 || y<0 || x>=n || y>=m || board[y][x]!=r_ch) return;
         
-        visited.insert({x,y});
-        prevHeight = heights[y][x];
+        board[y][x] = ch;
         
-        dfs(heights, x+1, y, visited, prevHeight);
-        dfs(heights, x-1, y, visited, prevHeight);
-        dfs(heights, x, y+1, visited, prevHeight);
-        dfs(heights, x, y-1, visited, prevHeight);
+        capture(board, x+1, y, ch, r_ch);
+        capture(board, x-1, y, ch, r_ch);
+        capture(board, x, y+1, ch, r_ch);
+        capture(board, x, y-1, ch, r_ch);
     }
 };
 
+
+
 int main() {
     Solution s;
-    vector<vector<int>> heights = {
-        {1,2,2,3,5},
-        {3,2,3,4,4},
-        {2,4,5,3,1},
-        {6,7,1,4,5},
-        {5,1,1,2,4},
-    };
-    s.pacificAtlantic(heights);
+    vector<vector<char>> board = {{ 'X', 'X', 'X', 'X' }, { 'X', 'O', 'O', 'X' }, { 'X', 'X', 'O', 'X' }, { 'X', 'O', 'X', 'X' } };
+    s.solve(board);
+    cout<<"board: \n";
+    for(auto i : board) {
+        for(auto j : i) cout<<j<<" "; cout<<endl;
+    }
+    cout<<"\nboard after solving: \n";
+    for(auto i : board) {
+        for(auto j : i) cout<<j<<" "; cout<<endl;
+    }
+
     return 0;
 }
