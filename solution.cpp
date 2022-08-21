932149358167ca5694aa483850fff1652eb270c4
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-21 09:28:29 +0530

    286. Walls and Gates

--- solution.cpp
+++ solution.cpp
@@ -1,61 +1,72 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct Pos {
+/*
+INF -> room
+0 -> gate
+-1 -> wall
+*/
+
+#define INF INT_MAX
+
+struct pos {
     int x;
     int y;
-    Pos(int x=0, int y=0): x(x), y(y) {}
+    pos(int x=0, int y=0): x(x), y(y) {}
 };
 
 class Solution {
-    int m, n;  // m -> height, n -> width   (of grid)
-public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        m = grid.size(), n = grid[0].size();
-        int freshOrange = 0;
-        queue<Pos> que;
-        
-        for(int y=0; y<m; y++) {
-            for(int x=0; x<n; x++) {
-                if(grid[y][x]==1) ++freshOrange;
-                else if(grid[y][x]==2) que.push({x,y});
-            }
-        }
-        int time = bfs(grid, que, freshOrange);
-        return time;
+    int m, n;  // m -> height, n -> width   (of matrix)
+public: 
+    vector<vector<int>> wallsAndGates(vector<vector<int>>& rooms) {
+        m = rooms.size(), n = rooms[0].size();
+        queue<pos> que;
+        for(int y=0; y<m; y++) for(int x=0; x<n; x++) if(rooms[y][x] == 0) que.push({x,y}); // store all gates position
+        dfs(rooms,que);
+        return rooms;
     }
-    
-    int bfs(vector<vector<int>>& grid, queue<Pos>& que, int freshOrange) {
-        int time=0;
-        vector<Pos> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+    void dfs(vector<vector<int>>& rooms, queue<pos> que) {
+        int distance = 1;
+        vector<pos> direction = {{1,0}, {-1,0}, {0,1}, {0,-1}};
         while(!que.empty()) {
             int len = que.size();
-            for(int i=0; i<len; i++) {
+            for(int i=0;i<len;i++) {
                 int r = que.front().y, c = que.front().x; que.pop();
-                for(auto d : directions) {
-                    int x = c + d.x,  y = r + d.y; 
-                    if(x<0 || y<0 || x>=n || y>=m || grid[y][x] != 1) continue;
-                    grid[y][x] = 2;
-                    --freshOrange;
+                for(auto d : direction) {
+                    int x = c + d.x, y = r + d.y;
+                    if(x<0 || y<0 || x>=n || y>=m || rooms[y][x]!=INF) continue;
+                    rooms[y][x] = distance;
                     que.push({x,y});
                 }
             }
-            if(que.size()!=0) ++time;
+            ++distance;
         }
-       
-        return (freshOrange == 0) ? time : -1;
     }
 };
 
 
+void printRooms(vector<vector<int>>& rooms) {
+    for(auto i : rooms) {
+        for (auto j : i) {
+            if(j>-1) cout<<" ";
+            if (j == INF) cout << "I ";
+            else cout << j << " ";
+        }
+        cout<<endl;
+    }
+}
+
 int main() {
     Solution s;
-    vector<vector<int>> grid = {
-        {2,1,1},
-        {1,1,0},
-        {0,1,1},
+    vector<vector<int>> rooms = {
+        {INF, -1,    0, INF},
+        {INF, INF, INF,  -1},
+        {INF, -1,  INF,  -1},
+        {0,   -1,  INF, INF},
     };
-    cout<<s.orangesRotting(grid)<<endl;
-
+    s.wallsAndGates(rooms);
+    printRooms(rooms);
     return 0;
 }
+
