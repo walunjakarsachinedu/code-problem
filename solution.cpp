1748c8225b798ad3028fd835e04c39d045997711
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-21 08:51:20 +0530

    994. Rotting Oranges

--- solution.cpp
+++ solution.cpp
@@ -1,46 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct Pos {
+    int x;
+    int y;
+    Pos(int x=0, int y=0): x(x), y(y) {}
+};
+
 class Solution {
-    int m, n; // m -> height, n -> width   (of board)
+    int m, n;  // m -> height, n -> width   (of grid)
 public:
-    void solve(vector<vector<char>>& board) {
-        m=board.size(), n=board[0].size();
+    int orangesRotting(vector<vector<int>>& grid) {
+        m = grid.size(), n = grid[0].size();
+        int freshOrange = 0;
+        queue<Pos> que;
         
-        for(int x=0; x<n; x++) capture(board, x, 0, 'T'), capture(board, x, m-1, 'T');
-        for(int y=0; y<m; y++) capture(board, 0, y, 'T'), capture(board, n-1, y, 'T');
-        
-        for(int y=0; y<m; y++) for(int x=0; x<n; x++) capture(board, x, y);        
-        
-        for(int y=0; y<m; y++) for(int x=0; x<n; x++) if(board[y][x]=='T') board[y][x] = 'O';        
+        for(int y=0; y<m; y++) {
+            for(int x=0; x<n; x++) {
+                if(grid[y][x]==1) ++freshOrange;
+                else if(grid[y][x]==2) que.push({x,y});
+            }
+        }
+        int time = bfs(grid, que, freshOrange);
+        return time;
     }
     
-    void capture(vector<vector<char>>& board, int x, int y, char ch='X', char r_ch='O') { 
-        if(x<0 || y<0 || x>=n || y>=m || board[y][x]!=r_ch) return;
-        
-        board[y][x] = ch;
-        
-        capture(board, x+1, y, ch, r_ch);
-        capture(board, x-1, y, ch, r_ch);
-        capture(board, x, y+1, ch, r_ch);
-        capture(board, x, y-1, ch, r_ch);
+    int bfs(vector<vector<int>>& grid, queue<Pos>& que, int freshOrange) {
+        int time=0;
+        vector<Pos> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        while(!que.empty()) {
+            int len = que.size();
+            for(int i=0; i<len; i++) {
+                int r = que.front().y, c = que.front().x; que.pop();
+                for(auto d : directions) {
+                    int x = c + d.x,  y = r + d.y; 
+                    if(x<0 || y<0 || x>=n || y>=m || grid[y][x] != 1) continue;
+                    grid[y][x] = 2;
+                    --freshOrange;
+                    que.push({x,y});
+                }
+            }
+            if(que.size()!=0) ++time;
+        }
+       
+        return (freshOrange == 0) ? time : -1;
     }
 };
 
 
-
 int main() {
     Solution s;
-    vector<vector<char>> board = {{ 'X', 'X', 'X', 'X' }, { 'X', 'O', 'O', 'X' }, { 'X', 'X', 'O', 'X' }, { 'X', 'O', 'X', 'X' } };
-    s.solve(board);
-    cout<<"board: \n";
-    for(auto i : board) {
-        for(auto j : i) cout<<j<<" "; cout<<endl;
-    }
-    cout<<"\nboard after solving: \n";
-    for(auto i : board) {
-        for(auto j : i) cout<<j<<" "; cout<<endl;
-    }
+    vector<vector<int>> grid = {
+        {2,1,1},
+        {1,1,0},
+        {0,1,1},
+    };
+    cout<<s.orangesRotting(grid)<<endl;
 
     return 0;
 }
