1f6cbe51fd77e2c6c81726476dee410eb4367438
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-22 16:21:33 +0530

    207. Course Schedule

--- solution.cpp
+++ solution.cpp
@@ -1,72 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 /*
-INF -> room
-0 -> gate
--1 -> wall
+algorithm: each course should form uncycled graph with its prerequisite
+important note: to make this algorithm to work, use backtracking 
 */
-
-#define INF INT_MAX
-
-struct pos {
-    int x;
-    int y;
-    pos(int x=0, int y=0): x(x), y(y) {}
-};
-
 class Solution {
-    int m, n;  // m -> height, n -> width   (of matrix)
-public: 
-    vector<vector<int>> wallsAndGates(vector<vector<int>>& rooms) {
-        m = rooms.size(), n = rooms[0].size();
-        queue<pos> que;
-        for(int y=0; y<m; y++) for(int x=0; x<n; x++) if(rooms[y][x] == 0) que.push({x,y}); // store all gates position
-        dfs(rooms,que);
-        return rooms;
-    }
-
-    void dfs(vector<vector<int>>& rooms, queue<pos> que) {
-        int distance = 1;
-        vector<pos> direction = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-        while(!que.empty()) {
-            int len = que.size();
-            for(int i=0;i<len;i++) {
-                int r = que.front().y, c = que.front().x; que.pop();
-                for(auto d : direction) {
-                    int x = c + d.x, y = r + d.y;
-                    if(x<0 || y<0 || x>=n || y>=m || rooms[y][x]!=INF) continue;
-                    rooms[y][x] = distance;
-                    que.push({x,y});
-                }
-            }
-            ++distance;
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        map<int, set<int>> preMap;
+        for(auto pre : prerequisites) preMap[pre[0]].insert(pre[1]);
+        vector<bool> visiting(numCourses, false);
+        for(int course=0; course<numCourses; course++) {
+            if(!dfs(course, preMap, visiting)) return false;
         }
+        return true;
+    }
+    
+    bool dfs(int course, map<int,set<int>>& preMap, vector<bool>& visiting) {
+        if(visiting[course]) return false;
+        if(preMap[course].empty()) return true;
+        
+        visiting[course] = true;
+        auto prerequisites = preMap[course];
+        for(auto pre : prerequisites)  
+            if(!dfs(pre, preMap, visiting)) return false;
+        visiting[course] = false;
+        
+        preMap[course].clear();
+        
+        return true;
     }
 };
 
-
-void printRooms(vector<vector<int>>& rooms) {
-    for(auto i : rooms) {
-        for (auto j : i) {
-            if(j>-1) cout<<" ";
-            if (j == INF) cout << "I ";
-            else cout << j << " ";
-        }
-        cout<<endl;
-    }
-}
 
 int main() {
+    int numCourses = 4;
+    vector<vector<int>> prerequisites = {{0,1}, {0,2}, {1,3}, {2,3}};
     Solution s;
-    vector<vector<int>> rooms = {
-        {INF, -1,    0, INF},
-        {INF, INF, INF,  -1},
-        {INF, -1,  INF,  -1},
-        {0,   -1,  INF, INF},
-    };
-    s.wallsAndGates(rooms);
-    printRooms(rooms);
+    cout<<s.canFinish(numCourses, prerequisites);
+    cout<<endl;
     return 0;
 }
 
