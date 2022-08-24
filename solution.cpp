2a1acedf7293e55f4e4bc398887d74e46c76be31
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-24 09:35:36 +0530

    323. Number of Connected Components in an Undirected Graph

--- solution.cpp
+++ solution.cpp
@@ -1,52 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution { // UnionFind Alogrithm Is Used
+class Solution {
 public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int total_nodes = edges.size()+1;
-        int rank[total_nodes]; // size of tree with node as root
-        int parent[total_nodes];
-        for(int node=0;node<total_nodes;node++) {
+    int countComponents(int n, vector<vector<int>>& edges) {
+        int parent[n];
+        int rank[n];
+        for(int node=0; node<n; node++) {
+            parent[node] = n;
             rank[node] = 1;
-            parent[node] = node;
         }
-        
-        for(auto edge : edges) {
-            if(!unionNode(edge[0], edge[1], parent, rank)) return edge;
-        }
-        return {1,1}; // this statement will not be reach
+
+        int count = n;
+        for(auto edge : edges) count -= unionNode(edge[0], edge[1], parent, rank);
+
+        return count;
     }
 
- private:   
+private:
     int findRoot(int node, int parent[]) {
-        while(parent[node] != node) {
-            parent[node] = parent[parent[node]];
-            node = parent[node];
-        }
+        while(node == parent[node]) node = parent[node] = parent[parent[node]];
         return node;
     }
-    
-    bool unionNode(int node1, int node2, int parent[], int rank[]) {
+
+    int unionNode(int node1, int node2, int parent[], int rank[]) {
         node1 = findRoot(node1, parent);
         node2 = findRoot(node2, parent);
-        if(node1 == node2) return false;
-        if(rank[node1] > rank[node2]) {
+
+        if(node1==node2) return 0;
+
+        if(rank[node1] >= rank[node2]) {
             parent[node2] = node1;
             rank[node1] += rank[node2];
         } else {
             parent[node1] = node2;
             rank[node2] += rank[node1];
         }
-        return true;
+        return 1;
     }
 };
 
 
 int main() {
     Solution s;
-    vector<vector<int>> edges = {{1,2},{2,3},{3,4},{1,4},{1,5}};
-    vector<int> redundantEdge = s.findRedundantConnection(edges);
-    cout<<redundantEdge[0]<<"-"<<redundantEdge[1]<<endl;
+    vector<vector<int>> edges = {{0,1}, {1,2}, {3,4}};
+    int count = s.countComponents(5,edges);
+    cout<<'\n'<<count<<'\n';
     return 0;
 }
