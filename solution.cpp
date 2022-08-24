97f78a14afab99f8d8387671a9da8cf9a6567f48
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-08-24 06:57:21 +0530

    684. Redundant Connection

--- solution.cpp
+++ solution.cpp
@@ -1,71 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution { // UnionFind Alogrithm Is Used
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> preMap[numCourses];
-        for (auto pre : prerequisites)
-            preMap[pre[1]].push_back(pre[0]);
-        
-        vector<int> dependency(numCourses, 0);
-        for(auto pre : prerequisites)
-            ++dependency[pre[0]];
-        
-        queue<int> que;
-        for(int i=0;i<numCourses;i++) {
-            if(!dependency[i]) que.push(i);
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int total_nodes = edges.size()+1;
+        int rank[total_nodes]; // size of tree with node as root
+        int parent[total_nodes];
+        for(int node=0;node<total_nodes;node++) {
+            rank[node] = 1;
+            parent[node] = node;
         }
-
-        vector<int> result;  result.reserve(numCourses);
-        while(!que.empty()) {
-            int course = que.front(); que.pop();
-            for(auto i : preMap[course]) {
-                --dependency[i];
-                if(!dependency[i]) que.push(i);
-            }
-            result.emplace_back(course);
+        
+        for(auto edge : edges) {
+            if(!unionNode(edge[0], edge[1], parent, rank)) return edge;
         }
+        return {1,1}; // this statement will not be reach
+    }
 
-        if(numCourses != result.size()) return {};
-        return result;
+ private:   
+    int findRoot(int node, int parent[]) {
+        while(parent[node] != node) {
+            parent[node] = parent[parent[node]];
+            node = parent[node];
+        }
+        return node;
     }
     
+    bool unionNode(int node1, int node2, int parent[], int rank[]) {
+        node1 = findRoot(node1, parent);
+        node2 = findRoot(node2, parent);
+        if(node1 == node2) return false;
+        if(rank[node1] > rank[node2]) {
+            parent[node2] = node1;
+            rank[node1] += rank[node2];
+        } else {
+            parent[node1] = node2;
+            rank[node2] += rank[node1];
+        }
+        return true;
+    }
 };
 
+
 int main() {
-    int numCourses = 4;
-    vector<vector<int>> prerequisites = {{0,1}, {0,2}, {1,3}, {2,3}};
     Solution s;
-    vector<int>  a = s.findOrder(4,prerequisites);
-    for(auto i : a) cout<<i<<" "; cout<<endl;
+    vector<vector<int>> edges = {{1,2},{2,3},{3,4},{1,4},{1,5}};
+    vector<int> redundantEdge = s.findRedundantConnection(edges);
+    cout<<redundantEdge[0]<<"-"<<redundantEdge[1]<<endl;
     return 0;
 }
-
-
-
-/*
-    0   
-  1   2
-    3   4
-    
-    3   4
-  1   2
-    0
-
-order : 3 1 2 0
-
-adj : 0 : 1 2
-1 : 3
-2 : 3
-
-reverse-adj : 3 : 1 2
-1 : 0
-2 : 0
-
-visitedCount : 
-0 : 2
-1 : 1 
-2 : 1
-3 : 0
-*/
