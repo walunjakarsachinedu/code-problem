abdfac1595c229b4df4a5a6dfbfe259b99fc27d9
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-09-16 07:32:22 +0530

    787. Cheapest Flights Within K Stops

--- solution.cpp
+++ solution.cpp
@@ -4,48 +4,39 @@ using namespace std;
 
 class Solution {
 public:
-    /**
-     * @param words: a list of words
-     * @return: a string which is correct order
-     */
-    string alienOrder(vector<string> &words) {
-        map<char, vector<char>> graph;
+    struct Edge {
+        int val, cost;
+        Edge() {}
+        Edge(int dst, int cost) : val(dst), cost(cost) {}
+    };
 
-        for(auto word : words) for(auto ch : word) graph[ch] = {};
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<Edge>> graph(n); // node->list of edges
+        for(auto flight : flights) graph[flight[0]].push_back({flight[1], flight[2]});
 
-        for(int i=0; i<words.size()-1; i++) {
-            string w1 = words[i], w2 = words[i+1];
-            int len = min(w1.size(), w2.size());
-            if(w1.size() > w2.size() && w1.substr(0, len) == w2) return "";
-            for(int j=0; j<len; j++) {
-                char src = w1[j], dst = w2[j];
-                if(src == dst) continue;
-                graph[src].push_back(dst);
-                break;
+        queue<Edge> que;
+        vector<int> dist(n, INT_MAX);
+        que.push({src, 0});
+        int stops = -1;
+        while(stops <= k && !que.empty()) {
+            int len = que.size();
+            for(int i=0; i<len; i++) {
+                Edge n = que.front();
+                que.pop();
+                if(n.cost > dist[n.val]) continue;
+                dist[n.val] = n.cost;
+                for(Edge nei : graph[n.val]) que.push({nei.val, nei.cost + n.cost});
             }
+            ++stops;
         }
-        
-        priority_queue<char, vector<char>, greater<char>> que;
-        map<char, int> indegree;
-        for(auto node : graph) for(auto nei : node.second) ++indegree[nei];
-        for(auto node : graph) if(!indegree[node.first]) que.push(node.first);
-        string order = "";
-        while(!que.empty()) {
-            auto cur = que.top(); que.pop();
-            order += cur;
-            for(auto neighbour : graph[cur]) {
-                --indegree[neighbour];
-                if(!indegree[neighbour]) que.emplace(neighbour);
-            }
-        }
-        return order.size() < graph.size() ? "" : order;
+        return dist[dst] == INT_MAX ? -1 : dist[dst];
     }
 };
 
 int main() {
+    vector<vector<int>> flights = {{0,1,100},{1,2,100},{2,0,100},{1,3,600},{2,3,200}};
     Solution s;
-    vector<string> words = {"zy", "zx"};
-    string order = s.alienOrder(words);
-    for(auto ch: order) cout<<ch<<" "; cout<<endl;
+    int cheapestPrice = s.findCheapestPrice(4, flights, 0, 3, 1);
+    cout << cheapestPrice << endl;
     return 0;
 }
