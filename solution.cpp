3e35d48e86089a14bd56272c283bd43e528aa169
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-11-10 19:00:33 +0530

    53. Maximum Subarray

--- solution.cpp
+++ solution.cpp
@@ -4,33 +4,21 @@ using namespace std;
 
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-      if(intervals.size() == 0) return 0;
-      auto comparator = [](const vector<int>& a, const vector<int>& b) {
-        return a[1] < b[1];
-      };
-      sort(intervals.begin(), intervals.end(), comparator);
-      int end = intervals[0][1];
-      int count = 1;
-
-      for(int i=1; i<intervals.size(); i++) {
-        if(intervals[i][0] >= end) {
-          ++count;
-          end = intervals[i][1];
+    int maxSubArray(vector<int>& nums) {
+        int result = nums[0];
+        int cur_sum = nums[0];
+        for(int i=1; i<nums.size(); i++) {
+          cur_sum = max(cur_sum, 0);
+          cur_sum += nums[i];
+          result = max(result, cur_sum);
         }
-      }
-      return intervals.size() - count;
+        return result;
     }
 };
 
 int main() {
-  vector<vector<int>> intervals = {
-    {1,3},
-    {1,2},
-    {2,3},
-    {3,4}
-  };
   Solution s;
-  cout << s.eraseOverlapIntervals(intervals);
+  vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+  s.maxSubArray(nums);
   return 0;
 }
