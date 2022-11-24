a5aaa53036bf8a8412df7d634af3aafe2120440c
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-11-24 06:26:06 +0530

    279. Perfect Squares

--- solution.cpp
+++ solution.cpp
@@ -1,58 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include "print.cpp"
 using namespace std;
 
-// 1926. Nearest Exit from Entrance in Maze
+// 279. Perfect Squares
 class Solution {
 public:
-    int nearestExit(vector<vector<char>>& maze, vector<int>& e) {
-      int R = maze.size();
-      int C = maze[0].size();
-      vector<pair<int,int>> direction =  {{1,0}, {0,1}, {-1,0}, {0,-1}};
-      queue<pair<int,int>>  que;
-      que.push({e[0], e[1]});
-      maze[e[0]][e[1]] = '+'; // mark visited
-
-      for(int r=0; r<R; r++) {
-        if(maze[r][0] == '.') maze[r][0] = 'e';
-        if(maze[r][C-1] == '.') maze[r][C-1] = 'e';
-      }
-      for(int c=0; c<C; c++) {
-        if(maze[0][c] == '.') maze[0][c] = 'e';
-        if(maze[R-1][c] == '.') maze[R-1][c] = 'e';
-      }
-
-      int step = 1;
-      while(!que.empty()) {
-        int levelSize = que.size();
-        for(int i=0; i<levelSize; i++) {
-          auto [r,c] = que.front(); que.pop();
-          for(auto [dr, dc] : direction) {
-            dr += r, dc += c;
-            if(dr < 0 || dc < 0 || dr >= R || dc == C || maze[dr][dc] == '+') continue;
-            que.push({dr, dc});
-            //if(dr == 0 || dc == 0 || dr == R-1 || dc== C-1) 
-            if(maze[dr][dc] == 'e') return step;
-            maze[dr][dc] = '+'; // mark visited
-          }
-        }
-        ++step;
-      }
-      return -1;
+  int numSquares(int n) {
+    vector<int> cache(n+1, -1);
+    return helper(n, cache);
+  }
+  int helper(int n, vector<int>& cache) {
+    if(cache[n] != -1) return cache[n];
+    if(n<=3) return n;
+    int minPath = n-1;
+    for(int i=1,j=1; j<=n; ++i, j=i*i) {
+      minPath = min(minPath, helper(n-j, cache));
     }
+    minPath += 1;
+    cache[n] = minPath;
+    return minPath;
+  }
 };
 
 int main() {
-  vector<int> entrance = {1,0};
-  vector<vector<char>> maze = {
-    {'+','+','+','+'},
-    {'.','.','+','+'},
-    {'+','.','.','+'},
-    {'+','+','.','+'}
-  };
-  Solution s;
-  cout << s.nearestExit(maze, entrance) << endl;
-  return 0;
+  cout << Solution().numSquares(50) << endl;
 }
-
-
