b5617f02b7db587df692cb6be0463ecf0a2eb135
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-11-24 18:45:26 +0530

    209. Minimum Size Subarray Sum

--- solution.cpp
+++ solution.cpp
@@ -3,32 +3,35 @@
 using namespace std;
 
 
-// 1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold
+// 209. Minimum Size Subarray Sum
 class Solution {
 public:
-    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
-        int count = 0;
-        float avg = 0;
-        int l = 0;
-        for(int r=0;r<arr.size();r++) {
-          avg += (float) arr[r]/k;
-          if(r-l+1 == k) {
-            if(avg >=threshold) ++count;
-            avg -= (float) arr[l]/k;
-            ++l;
-          }
+    int minSubArrayLen(int target, vector<int>& nums) {
+      int l=0;
+      int minLen = INT_MAX;
+      int curSum = 0;
+      for(int r=0; r<nums.size(); r++) {
+        curSum += nums[r];
+        while(curSum >= target && l <= r) {
+          minLen = min(minLen, r-l+1);
+          curSum -= nums[l++];
         }
-        return count;
+      }
+
+      if(minLen == INT_MAX) return 0;
+      return minLen;
     }
 };
 
 
-int main() {
-  vector<int> arr = {2,2,2,2,5,5,5,8};
-  int k = 3;
-  int threshold = 4;
 
-  int count = Solution().numOfSubarrays(arr, k, threshold);
-  cout << count << endl;
+int main() {
+  vector<int> nums = {1,4,4};
+  int target = 4;
+  int minlen = Solution().minSubArrayLen(2, nums);
+
+  cout << minlen << endl;
+
   return 0;
 }
+
