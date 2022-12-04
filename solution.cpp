13aee39a92c37ba2ade9291709c605f06d66bb5a
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-12-04 14:20:53 +0530

    125. Valid Palindrome

--- solution.cpp
+++ solution.cpp
@@ -1,46 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include "print.cpp"
 using namespace std;
 
-
-// 3. Longest Substring Without Repeating Characters
+// 125. Valid Palindrome
 class Solution {
 public:
-    // O(nlogn)
-    int lengthOfLongestSubstring(string s) { 
-      unordered_set<char> st;
-      int l=0;
-      int maxLen = 0;
-      for(int r=0; r<s.size(); ++r) {
-        while(st.find(s[r]) != st.end()) st.erase(s[l++]);
-        st.insert(s[r]);
-        maxLen = max(maxLen, r-l+1);
-      }
-      return maxLen;
-    }
-
-    // optimize solution : O(n)
-    int _lengthOfLongestSubstring(string s) {
-        // sym : last pos + 1
-        unordered_map<char, int> map;
-        int left = 0;
-        int maxLen = 0;
-
-        for (int i = 0; i < s.size(); i++) {
-            left = max(map[s[i]], left);
-            maxLen = max(maxLen, i - left + 1);
-            map[s[i]] = i + 1;
+    bool isPalindrome(string s) {
+        int l=0, r=s.size()-1;
+        while(l<r) {
+          if(!isAlpha(s[l])) ++l;
+          else if(!isAlpha(s[r])) --r;
+          else if(toSmall(s[l])!=toSmall(s[r])) 
+            return false;
+          else ++l, --r;
         }
-        return maxLen;
+        return true;
     }
 
-
+    bool isAlpha(char c) {
+      return (c>='a' && c <='z') || (c>='A' && c<='Z') || (c>='0' && c<='9');
+    }
+    char toSmall(char c) {
+      return (c>='A' && c<='Z') ? c + 32 : c;
+    }
 };
 
-
 int main() {
-  int len = Solution().lengthOfLongestSubstring("abcabcbb");
-  cout << len << endl;
+  
   return 0;
 }
-
