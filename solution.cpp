179ade34c69efca212ad5a7b94fc755254414d78
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-12-04 16:44:12 +0530

    80. Remove Duplicates from Sorted Array II

--- solution.cpp
+++ solution.cpp
@@ -2,15 +2,22 @@
 #include "print.cpp"
 using namespace std;
 
-// 26. Remove Duplicates from Sorted Array
+// 80. Remove Duplicates from Sorted Array II
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-      int count = 1, l=0;
-      for (int r=1; r<nums.size(); ++r) {
-        if (nums[r] > nums[l]) {
+      int l=0, count=1;
+      bool isDublicate = false;
+      for(int r=1; r<nums.size(); ++r) {
+        if (!isDublicate && nums[r]==nums[l]) {
+          isDublicate = true;
+          nums[++l] = nums[r];
           ++count;
-          nums[++l] = nums[r]; 
+        }
+        else if(nums[r] > nums[l]) {
+          isDublicate = false;
+          nums[++l] = nums[r];
+          ++count;
         }
       }
       cout << "nums: " << nums << endl;
@@ -19,7 +26,7 @@ public:
 };
 
 int main() {
-  vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
+  vector<int> nums = {1,1,1,2,2,3};
   int k = Solution().removeDuplicates(nums);
   cout << "k: " << k << endl;
   return 0;
