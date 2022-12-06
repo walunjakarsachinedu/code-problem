fa93e3022f81be6a16aa48cba056413aae2d4274
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-12-06 09:23:09 +0530

    876. Middle of the Linked List

--- solution.cpp
+++ solution.cpp
@@ -2,37 +2,30 @@
 #include "print.cpp"
 using namespace std;
 
-// 2256. Minimum Average Difference
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// 876. Middle of the Linked List
 class Solution {
 public:
-    int minimumAverageDifference(vector<int>& nums) {
-        size_t sum = 0;
-        int N = nums.size();
-        for(int num: nums) sum += num;
-
-        int minIndex = 0;
-        int minDiff = INT_MAX;
-        size_t curSum = 0;
-        for(int i=0; i<N; i++) {
-          curSum += nums[i];
-          int curAvg = curSum/(i+1);
-          int nextAvg = (i==N-1) ? 0 : (sum - curSum) / (N-(i+1));
-          int curDiff = abs(curAvg - nextAvg);
-
-          if(curDiff < minDiff) {
-            minDiff = curDiff;
-            minIndex = i;
-          }
-        }
-        return minIndex;
+    ListNode* middleNode(ListNode* head) {
+      ListNode *sp = head, *fp = head;
+      while(fp!=NULL && fp->next!=NULL) {
+        sp = sp->next;
+        fp = fp->next->next;
+      }
+      return sp;
     }
 };
-// 2,5,3,9,5,3
-// sum = 27
 
 int main() {
-  vector<int> nums = {2,5,3,9,5,3}; 
-  int minDiff = Solution().minimumAverageDifference(nums);
-  cout << minDiff << endl;
+  ListNode* head = new ListNode(1, new ListNode(2, new ListNode(3)));
+  ListNode* middleNode = Solution().middleNode(head);
+  cout << "middle node: " << middleNode->val << endl;
   return 0;
 }
