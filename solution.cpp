36f3e1027fec69e375b1569b788b1868a67e0c62
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-12-15 09:05:47 +0530

    1143. Longest Common Subsequence

--- solution.cpp
+++ solution.cpp
@@ -2,28 +2,28 @@
 #include "print.cpp"
 using namespace std;
 
-// 198. House Robber
+// 1143. Longest Common Subsequence
 class Solution {
-  int N;
 public:
-  int rob(vector<int>& nums) {
-    N = nums.size();
-    vector<int> cache(N, -1);
-    return dp(nums, 0, 0, cache);
+  int longestCommonSubsequence(string t1, string t2) {
+    vector<vector<int>> cache(t1.size(), vector<int>(t2.size(), -1));
+    return dp(t1, t2, 0, 0, cache);
   }
 
-  int dp(vector<int>& nums, int i, int money, vector<int>& cache) {
-    if(i >= N) return money;
-    if(cache[i] != -1) return cache[i];
-    int maxAmt = dp(nums, i+2, nums[i], cache);
-    if(i+1 < N) maxAmt = max(maxAmt, dp(nums, i+3, nums[i+1], cache));
-    cache[i] = maxAmt + money;
-    return cache[i];
+  int dp(string& t1, string& t2, int i1, int i2, vector<vector<int>>& cache) {
+    if(i1 >= t1.size() || i2 >= t2.size()) return 0;
+    if(cache[i1][i2] != -1) return cache[i1][i2];
+    int length = 0;
+    if(t1[i1] == t2[i2]) length = 1 + dp(t1, t2, i1+1, i2+1, cache);
+    else length = max(dp(t1, t2, i1+1, i2, cache), dp(t1, t2, i1, i2+1, cache));
+    cache[i1][i2] = length;
+    return length;
   }
 };
 
 int main() {
-  vector<int> nums = {1,2,3,1};
-  cout << Solution().rob(nums) << endl;
+  string text1 = "abcde";
+  string text2 = "ace";
+  cout << Solution().longestCommonSubsequence(text1, text2) << endl;
   return 0;
 }
