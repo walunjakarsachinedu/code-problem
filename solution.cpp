56c0b4e7ba11db406281207cd2d69e8eea23c2c3
Sachin Walunjakar <walunjakarsachin@gmail.com>
2022-12-16 22:53:31 +0530

    232. Implement Queue using Stacks

--- solution.cpp
+++ solution.cpp
@@ -2,28 +2,37 @@
 #include "print.cpp"
 using namespace std;
 
-// 1143. Longest Common Subsequence
-class Solution {
+// 232. Implement Queue using Stacks
+class MyQueue {
 public:
-  int longestCommonSubsequence(string t1, string t2) {
-    vector<vector<int>> cache(t1.size(), vector<int>(t2.size(), -1));
-    return dp(t1, t2, 0, 0, cache);
-  }
+  stack<int> in, out;
 
-  int dp(string& t1, string& t2, int i1, int i2, vector<vector<int>>& cache) {
-    if(i1 >= t1.size() || i2 >= t2.size()) return 0;
-    if(cache[i1][i2] != -1) return cache[i1][i2];
-    int length = 0;
-    if(t1[i1] == t2[i2]) length = 1 + dp(t1, t2, i1+1, i2+1, cache);
-    else length = max(dp(t1, t2, i1+1, i2, cache), dp(t1, t2, i1, i2+1, cache));
-    cache[i1][i2] = length;
-    return length;
+  void push(int x) {
+    in.push(x);
+  }
+  
+  int pop() {
+    int top = peek(); out.pop();
+    return top;
+  }
+  
+  int peek() {
+    if(out.empty()) while(!in.empty()) out.push(in.top()), in.pop();
+    int top = out.top();
+    return top;
+  }
+  
+  bool empty() {
+    return in.empty() && out.empty();
   }
 };
 
 int main() {
-  string text1 = "abcde";
-  string text2 = "ace";
-  cout << Solution().longestCommonSubsequence(text1, text2) << endl;
+  MyQueue que;
+  que.push(1);
+  que.push(2);
+  cout << "top element in queue: " << que.peek() << endl;
+  cout << "poped element: " << que.pop() << endl;
+  cout << "is queue empty : " << (que.empty() ? "true" : "false") << endl;
   return 0;
 }
