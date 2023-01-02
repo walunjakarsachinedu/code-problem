81efef29083bb04444c305da0e65b1313dd15f15
Sachin Walunjakar <walunjakarsachin@gmail.com>
2023-01-02 10:07:29 +0530

    520. Detect Capital

--- solution.cpp
+++ solution.cpp
@@ -3,37 +3,32 @@
 using namespace std;
 
 
-// 309. Best Time to Buy and Sell Stock with Cooldown
+// 520. Detect Capital
 class Solution {
-  int N;
 public:
-  int maxProfit(vector<int>& prices) {
-    vector<vector<int>> cache(2, vector<int>(prices.size(), -1));
-    return dfs(prices, 0, true, cache);
-  }
-  
-  int dfs(vector<int>& prices, int i, bool isbuying, vector<vector<int>>& cache) {
-    if(i >= prices.size()) return 0;
-    if(cache[isbuying][i] != -1) return cache[isbuying][i];
-
-    if(isbuying) {
-      int buy = dfs(prices, i+1, !isbuying, cache) - prices[i]; // buy
-      int cooldown = dfs(prices, i+1, isbuying, cache); // cooldown
-      cache[isbuying][i] = max(buy, cooldown);
-      return max(buy, cooldown);
-    } 
-    else {
-      int sell = dfs(prices, i+2, !isbuying, cache) + prices[i]; // sell
-      int cooldown = dfs(prices, i+1, isbuying, cache); // cooldown
-      cache[isbuying][i] = max(sell, cooldown);
-      return max(sell, cooldown);
+    bool detectCapitalUse(string word) {
+      if(isSmall(word[0])) {
+        for(int i=1; i<word.size(); i++) {
+          if(!isSmall(word[i])) return false;
+        }
+      }
+      else {
+        bool isCase = word.size() > 1 && isSmall(word[1]);
+        for(int i=2; i<word.size(); i++)  {
+          if(isCase != isSmall(word[i])) return false;
+        }
+      }
+      return true;
     }
-  }
+
+    bool isSmall(char ch) {
+      return ch >= 'a' && ch <= 'z';
+    }
 };
 
 
 int main() {
-  vector<int> prices = {1,2,3,0,2};
-  cout << Solution().maxProfit(prices) << endl;
+  string word = "USA";
+  cout << Solution().detectCapitalUse(word) << endl;
   return 0;
 }
