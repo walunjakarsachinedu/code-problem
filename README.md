b30da3c48cf8e3a8542629cabba4010cac5ca4f0
Sachin Walunjakar <walunjakarsachin@gmail.com>
2023-01-12 20:39:18 +0530

    213. House Robber II

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### 198. House Robber
+### 213. House Robber II
 
 <br/>
 
@@ -6,7 +6,9 @@
 
 #### Algorithm: 
 ```
-def dp(i):
-  if(i>=nums.size) return 0;
-  return max(dp(i+1), dp(i+2) + nums[i]);
+def rob(nums):
+  def dp(i, size):
+    if(i>=size) return 0;
+    return max(dp(i+1), dp(i+2) + nums[i]);
+  return max(dp(0, nums.size-1), dp(1, nums.size));
 ```
--- solution.cpp
+++ solution.cpp
@@ -3,26 +3,27 @@
 using namespace std;
 
 
-// 198. House Robber
+// 213. House Robber II
 class Solution {
 public:
   int rob(vector<int>& nums) {
-    vector<int> cache(nums.size(), -1);
-    return dp(0, nums, cache);
+    if(nums.size() == 1) return nums[0];
+    vector<int> cache(nums.size(), -1), cache1(nums.size(), -1);
+    return max(dp(0, nums, cache, nums.size()-1), dp(1, nums, cache1, nums.size()));
   }
 
-  int dp(int i, vector<int>& nums, vector<int>& cache) {
-    if(i >= nums.size()) return 0;
+  int dp(int i, vector<int>& nums, vector<int>& cache, int size) {
+    if(i >= size) return 0;
     if(cache[i] != -1) return cache[i];
 
-    int include = dp(i+2, nums, cache) + nums[i];
-    int skip = dp(i+1, nums, cache);
+    int include = dp(i+2, nums, cache, size) + nums[i];
+    int skip = dp(i+1, nums, cache, size);
     return cache[i] = max(skip, include);
   }
 };
 
 int main() {
-  vector<int> nums = {1,2,3,1};
+  vector<int> nums = {1,2,3};
   cout << Solution().rob(nums) << endl;
   return 0;
 }
