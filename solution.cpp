30c95e74b57ffad97de369328c3964b0c8aaf8e9
Sachin Walunjakar <walunjakarsachin@gmail.com>
2023-07-02 11:20:39 +0530

    2305. Fair Distribution of Cookies

--- solution.cpp
+++ solution.cpp
@@ -1,65 +1,37 @@
-#include <bits/stdc++.h>
-#include "print.cpp"
+#include<vector>
+#include<iostream>
 using namespace std;
 
-class UnionFind;
-class UnionFind {
-  vector<int> graph;
-  vector<int> rank;
-public:
-  UnionFind(int n) {
-    rank = vector<int>(n, 0);
-    graph = vector<int>(n);
-    for(int i=0; i<n; i++) graph[i] = i;
-  }
 
-  int find(int node) {
-    while(node != graph[node]) node = graph[node] = graph[graph[node]];
-    return node;
-  }
-
-  void unions(int n1, int n2) {
-    n1 = find(n1);
-    n2 = find(n2);
-    if(n1 == n2) return;
-    if(rank[n1] >= rank[n2]) graph[n2] = n1, ++rank[n1];
-    else graph[n1] = n2, ++rank[n2];
-  }  
-};
-
-// 2421. Number of Good Paths
+// 2305. Fair Distribution of Cookies
 class Solution {
 public:
-  int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) {
-    vector<vector<int>> adj(vals.size());
-    for(auto& edge : edges) {
-      adj[edge[0]].push_back(edge[1]);
-      adj[edge[1]].push_back(edge[0]);
+    int distributeCookies(vector<int>& cookies, int k) {
+      vector<int> children(k,0);
+      return _rec(0, cookies, children);
     }
-
-    map<int, vector<int>> valToIndex; // vals -> [node]
-    for(int i=0; i<vals.size(); i++) valToIndex[vals[i]].push_back(i);
-
-    UnionFind unionFind(vals.size());
-    
-    int result = 0;
-    for(auto& [val, indexes] : valToIndex) {
-      for(int node : indexes) for(int nei : adj[node]) if(vals[nei] <= vals[node]) unionFind.unions(nei, node);
-      unordered_map<int, vector<int>> groups; // group -> [group node member]
-      for(int node : indexes) groups[unionFind.find(node)].push_back(node);
-      for(auto& [group, members] : groups) result += countGoodPaths(members.size());
+private:
+    int _rec(int i, vector<int>& cookies, vector<int>& children) {
+      if(i >= cookies.size()) return max_element(children);
+      int min_cookies = 1e6;
+      for(int c=0; c<children.size(); c++) {
+        children[c] += cookies[i];
+        min_cookies = min(_rec(i+1, cookies, children), min_cookies);
+        children[c] -= cookies[i];
+      }
+      return min_cookies;
     }
-    return result;
-  }
-
-  int countGoodPaths(int count) {
-    return count + count * (count - 1) / 2;
-  }
+    int max_element(vector<int>& children) {
+      int result = 0;
+      for(int child : children) result = max(child, result);
+      return result;
+    }
 };
 
 int main() {
-  vector<int> vals = {1,1,2,2,3}; 
-  vector<vector<int>> edges = {{0,1},{1,2},{2,3},{2,4}};
-  cout << Solution().numberOfGoodPaths(vals, edges) << endl;
-  return 0;
+    Solution s;
+    vector<int> cookies = {6,1,3,2,2,4,1,2};
+    int k = 3;
+    cout << s.distributeCookies(cookies, k) << endl;
+    return 0;
 }
